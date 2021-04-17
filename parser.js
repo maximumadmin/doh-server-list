fbc2e036a326f0e50ddfe6c42a8190349fc4ab8a
maximumadmin <61138453+maximumadmin@users.noreply.github.com>
2021-04-17 14:59:52 +0900

    Port parser to Node.js

--- parser.js
+++ parser.js
@@ -1,27 +1,26 @@
-const download = (filename, text) => {
-  const data = 'data:text/plain;charset=utf-8,' + encodeURIComponent(text)
-  const element = document.createElement('a')
-  element.setAttribute('href', data)
-  element.setAttribute('download', filename)
-  if (!document.createEvent) {
-    element.click()
-    return
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const parse = html => {
+  const $ = cheerio.load(html)
+  const table = $('#user-content-publicly-available-servers').parent().next()
+  const anchors = table.find('tbody tr > td:nth-child(2) a')
+  var hostnames = anchors.map((_, a) => new URL(a.attribs.href).hostname)
+  return [...new Set(hostnames)]
+}
+
+const main = async () => {
+  const url = 'https://github.com/curl/curl/wiki/DNS-over-HTTPS'
+  let response = null
+  try {
+    response = await axios.default.get(url)
+  } catch (e) {
+    console.error(e)
+    return 1
   }
-  const event = document.createEvent('MouseEvents')
-  event.initEvent('click', true, true)
-  element.dispatchEvent(event)
+  const hostnames = parse(response.data)
+  console.info(hostnames.join('\n'))
+  return 0
 }
 
-const parse = () => {
-  var elements = [
-    ...document
-      .getElementById('user-content-publicly-available-servers')
-      .parentElement
-      .nextElementSibling
-      .querySelectorAll('tbody tr > td:nth-child(2) a')
-  ]
-  var hostnames = elements.map(e => new URL(e.getAttribute('href')).hostname)
-  return [...new Set(hostnames)].join('\n')
-}
-
-download('list.txt', parse())
+main().then(code => process.exit(code))
